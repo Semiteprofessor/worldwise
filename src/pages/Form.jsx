afd5084b2fa3caf38d6c72ba63d2a4cc3841d6de
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-30 15:33:03 +0100

    Commit

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,19 +1,21 @@
+import styles from "./Form.module.css";
 const Form = () => {
   return (
-    <div>
-      <form>
-        <label>Name:</label>
-        <input type="text" name="name" />
-        <br />
-        <label>Email:</label>
-        <input type="email" name="email" />
-        <br />
-        <label>Message:</label>
-        <textarea name="message" rows="4" cols="50"></textarea>
-        <br />
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
+    <form className={styles.form}>
+      <label>City Name:</label>
+      <input type="text" name="city" />
+      <br />
+      <label>Whendid you go to ?</label>
+      <input type="email" name="email" />
+      <br />
+      <label>Notes about your trip to</label>
+      <textarea name="message" rows="4" cols="50"></textarea>
+      <br />
+      <div className={styles.button}>
+        <button className={styles.btn}>Add</button>
+        <button className={styles.btn}>Back</button>
+      </div>
+    </form>
   );
 };
 
