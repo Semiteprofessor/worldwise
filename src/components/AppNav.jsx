7939bde7bd9acf4e0dd5c508fa806dd1dc60e37b
Taiwo Olapade <semiteprofessor@gmail.com>
2024-07-27 16:16:40 +0100

    Commit

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,34 +1,19 @@
 import { NavLink } from "react-router-dom";
 import styles from "./AppNav.module.css";
-import { useState } from "react";
-const AppNav = () => {
-  const [toggleState, setToggleState] = useState(0);
 
-  const toggleTab = (index) => {
-    setToggleState(index);
-  };
-
-  const getActiveClass = (index, className) =>
-    toggleState === index ? className : {};
-
+function AppNav() {
   return (
-    <div className={styles.appNav}>
-      <ul className={styles.tabList}>
-        <li
-          className={`${styles.tabs} ${getActiveClass(0, styles.activeTabs)}`}
-          onClick={() => toggleTab(0)}
-        >
+    <nav className={styles.nav}>
+      <ul>
+        <li>
           <NavLink to="cities">Cities</NavLink>
         </li>
-        <li
-          className={`${styles.tabs} ${getActiveClass(1, styles.activeTabs)}`}
-          onClick={() => toggleTab(1)}
-        >
+        <li>
           <NavLink to="countries">Countries</NavLink>
         </li>
       </ul>
-    </div>
+    </nav>
   );
-};
+}
 
 export default AppNav;
